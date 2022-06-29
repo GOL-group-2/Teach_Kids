e67a75e8a813cbcdf4c5cd8cefa61166be46346b
franco9535 <87659308+franco9535@users.noreply.github.com>
2022-06-29 12:34:45 +0300

    Changing Mathematic into reading books

---
+++ javascript/about.js
@@ -0,0 +1,12 @@
+// const btn = document.getElementById('btn');
+
+// btn.addEventListener('click', function handleClick(event) {
+//   // 👇️ if you are submitting a form for booking
+//   event.preventDefault();
+
+//   const inputs = document.querySelectorAll('#school_name, #email_name, #teacher_name, #grade_name, #date_name, #time_name, #message_name');
+
+//   inputs.forEach(input => {
+//     input.value = '';
+//   });
+// });
